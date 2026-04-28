faf9cd333df1d740bd697ec2ea1d2fb50b0ac28f
Kurt M. Remmick <kremmick@cs.linfield.edu>
2026-04-28 14:55:04 -0700

    Changed order of if statement checking for plays of type comedy in for loop of statement.

--- statement_v0.js
+++ statement_v0.js
@@ -84,11 +84,9 @@ function statement(invoice, plays) {
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+      if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
       // print line for this order
-      result += ` ${play.name}: ${format(thisAmount / 100)} (${
-        perf.audience
-      } seats)\n`;
+      result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
       totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
